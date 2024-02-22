395144e731e91537e91311d7dc1eb0b0ee3df51a
Samwel Mburu <SamwelM@hollywoodbets.net>
2024-02-22 21:43:00 +0300

    Add CurrentTimeBackGroundJob

---
+++ Jobs/CurrentTimeBackGroundJob.cs
@@ -0,0 +1,18 @@
+﻿using Quartz;
+
+namespace THubWorkerService.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class CurrentTimeBackGroundJob : IJob
+    {
+        public CurrentTimeBackGroundJob()
+        {
+        }
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Serilog.Log.Information($"The current time is:{DateTime.UtcNow}");
+
+            await Task.CompletedTask;
+        }
+    }
+}
