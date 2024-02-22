b38cae1da2c8d903837ea02d53ae766e21e267e7
Samwel Mburu <SamwelM@hollywoodbets.net>
2024-02-22 21:45:07 +0300

    Add CurrentTimeBackGroundJobSetUp logic

---
+++ Jobs/Job Configuration/CurrentTimeBackGroundJobSetUp.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using Quartz;
+
+namespace THubWorkerService.Jobs.Job_Configuration
+{
+    public class CurrentTimeBackGroundJobSetUp : IConfigureOptions<QuartzOptions>
+    {
+        IConfiguration _configuration;
+        public CurrentTimeBackGroundJobSetUp(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void Configure(QuartzOptions options)
+        {
+            int _time = Convert.ToInt32(_configuration["Job_Interval"]);
+            var jobKey = JobKey.Create(nameof(CurrentTimeBackGroundJob));
+
+            options.AddJob<CurrentTimeBackGroundJob>(jobBuilder => jobBuilder.WithIdentity(jobKey))
+            .AddTrigger(trigger =>
+            trigger
+            .ForJob(jobKey)
+            .WithSimpleSchedule(schedule =>
+            schedule.WithIntervalInSeconds(_time).RepeatForever()));
+        }
+    }
+}
